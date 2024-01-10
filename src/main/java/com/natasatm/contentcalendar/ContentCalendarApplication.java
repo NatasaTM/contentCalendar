305c89382a1bd49e4dd6455167e73aa36e0e23a8
Natasa <natasa.t.markovic@gmail.com>
2024-01-10 22:53:03 +0100

    added home controller, edited pom.xml file

--- src/main/java/com/natasatm/contentcalendar/ContentCalendarApplication.java
+++ src/main/java/com/natasatm/contentcalendar/ContentCalendarApplication.java
@@ -1,5 +1,6 @@
 package com.natasatm.contentcalendar;
 
+import com.natasatm.contentcalendar.config.ContentCalendarProperties;
 import com.natasatm.contentcalendar.model.Content;
 import com.natasatm.contentcalendar.model.Status;
 import com.natasatm.contentcalendar.model.Type;
@@ -7,10 +8,11 @@ import com.natasatm.contentcalendar.repository.ContentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
-
+@EnableConfigurationProperties(ContentCalendarProperties.class)
 @SpringBootApplication
 public class  ContentCalendarApplication {
 
--- src/main/java/com/natasatm/contentcalendar/config/DataLoader.java
+++ src/main/java/com/natasatm/contentcalendar/config/DataLoader.java
@@ -1,9 +1,10 @@
 package com.natasatm.contentcalendar.config;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
-//@Controller
+//@Component
 public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
